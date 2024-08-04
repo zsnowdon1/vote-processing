5ae37a14d01eefe65130705ba350b9964c64abeb
Zack Snowdon <zack@Zacks-Air.lan>
2024-08-04 12:29:35 -0400

    Base repo

---
+++ src/main/java/com/voting/vote_processing/VoteProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.voting.vote_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteProcessingApplication.class, args);
+	}
+
+}
