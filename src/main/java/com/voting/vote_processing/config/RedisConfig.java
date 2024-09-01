03ab590aa7c69ae3ceb2d5a2edfa582734410423
zsnowdon <zacksnowdon5@gmail.com>
2024-09-01 17:16:03 -0400

    Working redis posting votes

--- src/main/java/com/voting/vote_processing/config/RedisConfig.java
+++ src/main/java/com/voting/vote_processing/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -27,6 +29,10 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
--- src/main/java/com/voting/vote_processing/service/KafkaConsumerServiceImpl.java
+++ src/main/java/com/voting/vote_processing/service/KafkaConsumerServiceImpl.java
@@ -1,19 +1,40 @@
 package com.voting.vote_processing.service;
 
+import com.voting.vote_processing.entity.SelectedChoice;
 import com.voting.vote_processing.entity.SurveyRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerServiceImpl implements KafkaConsumerService {
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerServiceImpl.class);
 
+    @Autowired
+    public KafkaConsumerServiceImpl(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
     @Override
-    @KafkaListener(topics = "votes-topic", groupId = "${spring.kafka.consumer.group-id}")
+    @KafkaListener(topics = "${spring.kafka.consumer.topic}", groupId = "${spring.kafka.consumer.group-id}")
     public void kafkaConsumer(Object message, SurveyRequest vote) {
-        logger.info("Received kafka message: {}", vote.toString());
+        logger.info("Received kafka message " + vote.toString());
+        long surveyId = vote.getSurveyId();
+        for(SelectedChoice choice: vote.getResponses()) {
+            logger.info("Processing vote for question " + choice.getQuestionId());
+            String questionId = Long.toString(choice.getQuestionId());
+            String choiceId = Long.toString(choice.getChoiceId());
+
+            String redisKey = "survey:" + surveyId + ":question:" + questionId + ":results";
+            logger.info("Incrementing votes for choice:" + choiceId);
+            redisTemplate.opsForHash().increment(redisKey, choiceId, 1);
+            logger.info("Processed vote for survey: " + surveyId + ", question: " + questionId + ", choice: " + choiceId);
+        }
     }
 }
